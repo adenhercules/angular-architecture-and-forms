53eb1af60cbe09054fb6bfea18784620ca3e998e
adenhercules <adenhercules07@gmail.com>
2022-12-26 18:47:21 -0400

    validacao padrao

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {NovoUsuarioService} from "./novo-usuario.service";
 import {NovoUsuario} from "./novo-usuario";
 
@@ -17,10 +17,10 @@ export class NovoUsuarioComponent implements OnInit {
     private novoUsuarioService: NovoUsuarioService
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      email: [''],
-      fullName: [''],
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, Validators.minLength(4)]],
       userName: [''],
       password: ['']
     });
