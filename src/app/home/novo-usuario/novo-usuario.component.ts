a004f70a624cbc5c51e85cb5c1ccc92aeb7a9a42
adenhercules <adenhercules07@gmail.com>
2022-12-26 19:33:21 -0400

    fazendo validacao de nome de usuario igual a senha

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -4,6 +4,7 @@ import {NovoUsuarioService} from "./novo-usuario.service";
 import {NovoUsuario} from "./novo-usuario";
 import {minusculoValidator} from "./minusculo.validator";
 import {UsuarioExisteService} from "./usuario-existe.service";
+import {usuarioSenhaIguaisValidator} from "./usuario-senha-iguais.validator";
 
 @Component({
   selector: 'app-novo-usuario',
@@ -26,7 +27,10 @@ export class NovoUsuarioComponent implements OnInit {
       fullName: ['', [Validators.required, Validators.minLength(4)]],
       userName: ['', [minusculoValidator],[this.usuarioExistenteService.usuarioJaExiste()]],
       password: ['']
-    });
+    },
+      {
+        validators: [usuarioSenhaIguaisValidator],
+      });
   }
 
   cadastrar(){
