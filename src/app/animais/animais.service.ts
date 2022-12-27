867822b1a5092b3a94bb1565a4c69a3b1e0d5763
adenhercules <adenhercules07@gmail.com>
2022-12-27 16:38:55 -0400

    serviico animais

---
+++ src/app/animais/animais.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Animais} from "./animais";
+import {TokenService} from "../autenticacao/token.service";
+const API = 'http://localhost: 300';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnimaisService {
+
+  constructor(
+    private http:HttpClient,
+    private tokenService: TokenService
+  ) {}
+
+  listaDoUsuario(nomeDoUsuairo: string): Observable<Animais>{
+    const token = this.tokenService.retornaToken();
+    const headers = new HttpHeaders().append('x-access-token', token)
+    return this.http.get<Animais>(`${API}/${nomeDoUsuairo}/photos`, {
+      headers
+    });
+  }
+}
