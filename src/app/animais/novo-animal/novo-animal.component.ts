f1bb056662394c3be6fded293622c7dac05eb736
adenhercules <adenhercules07@gmail.com>
2023-01-01 16:47:07 -0400

    compo de preview do upload

--- src/app/animais/novo-animal/novo-animal.component.ts
+++ src/app/animais/novo-animal/novo-animal.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-novo-animal',
   templateUrl: './novo-animal.component.html',
   styleUrls: ['./novo-animal.component.css']
 })
-export class NovoAnimalComponent {
+export class NovoAnimalComponent implements OnInit{
+  formularioAnimal !: FormGroup;
+  file !: File;
+  preview!: string;
+  percentualConcluido = 0;
 
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  uplaod(){
+
+  }
+  gravaArquivo(arquivo: any){
+
+  }
 }
