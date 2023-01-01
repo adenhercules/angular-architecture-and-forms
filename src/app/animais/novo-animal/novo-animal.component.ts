f2a25a32d4668718d16ebd77837097c0419a1fbf
adenhercules <adenhercules07@gmail.com>
2023-01-01 17:32:21 -0400

    correcoes de formatacao

--- src/app/animais/novo-animal/novo-animal.component.ts
+++ src/app/animais/novo-animal/novo-animal.component.ts
@@ -10,7 +10,7 @@ import {HttpEvent, HttpEventType} from "@angular/common/http";
   templateUrl: './novo-animal.component.html',
   styleUrls: ['./novo-animal.component.css']
 })
-export class NovoAnimalComponent implements OnInit{
+export class NovoAnimalComponent implements OnInit {
   formularioAnimal !: FormGroup;
   file !: File;
   preview!: string;
@@ -20,7 +20,8 @@ export class NovoAnimalComponent implements OnInit{
     private animaisService: AnimaisService,
     private formBuilder: FormBuilder,
     private router: Router
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
     this.formularioAnimal = this.formBuilder.group({
@@ -30,8 +31,7 @@ export class NovoAnimalComponent implements OnInit{
     })
   }
 
-
-  uplaod(){
+  uplaod() {
     const allowComments = this.formularioAnimal.get('allowComments')?.value ?? false;
     const description = this.formularioAnimal.get('description')?.value ?? '';
 
@@ -39,19 +39,21 @@ export class NovoAnimalComponent implements OnInit{
       .upload(description, allowComments, this.file)
       .pipe(
         finalize(() => this.router.navigate(['animais']))
-      ).subscribe((event: HttpEvent<any>)=>{
-        if (event.type == HttpEventType.UploadProgress){
+      ).subscribe((event: HttpEvent<any>) => {
+        if (event.type == HttpEventType.UploadProgress) {
           const total = event.total ?? 1;
-          this.percentualConcluido = Math.round(100*(event.loaded/total))
+          this.percentualConcluido = Math.round(100 * (event.loaded / total))
         }
-    },
+      },
       error => console.log(error));
   }
+
   gravaArquivo(arquivo: any): void {
     const [file] = arquivo?.files;
     this.file = file;
     const reader = new FileReader();
-    reader.onload = (event: any) =>(this.preview = event.target.result);
+    reader.onload = (event: any) => (this.preview = event.target.result);
     reader.readAsDataURL(file);
   }
+
 }
